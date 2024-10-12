732c62aa7ff5dd9351b25715d52b1a1c9d82dd06
zazzix <rusmaniak@gmail.com>
2024-10-12 16:26:42 +0000

    feat: add getUserInput, calculateCurrency functions

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDtoEUR = 0.91 * 1
 	const USDtoRUB = 96.20 * 1
 	const EURtoRUB = USDtoEUR * USDtoRUB
+	getUserInput()
+}
+
+func getUserInput() {
+	var currency string
+	fmt.Scan(&currency)
+	fmt.Println(currency)
+}
+
+func calculateCurrency(amount float64, currency1 string, currency2 string) float64 {
+
 }
